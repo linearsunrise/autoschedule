8dc09a49b87dc751622c892c1596cdb7aef8e9bd
Ilya Mordvinkin <ilya_mordvinkin@mail.ru>
2021-02-12 02:43:31 +0400

    feat: add timestamp format and ical events

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const fs = require('fs').promises;
 
 const TIMEZONE = '+04:00';
-const STARTDATE = [2021,02,01];
+const STARTDATE = [2021, 02, 01];
+const ENDPERIOD = ''
 
 const SCHEDULE = {
-    1: ["08:30", "10:05"],
-    2: ["10:15", "11:50"],
-    3: ["12:50", "14:25"],
-    4: ["14:35", "16:10"],
-    5: ["16:20", "17:55"]
+	1: ["08:30", "10:05"],
+	2: ["10:15", "11:50"],
+	3: ["12:50", "14:25"],
+	4: ["14:35", "16:10"],
+	5: ["16:20", "17:55"]
 };
 
 const DAYOFTHEWEEK = {
-    "Понедельник": 01,
-    "Вторник": 02,
-    "Среда": 03,
-    "Четверг": 04,
-    "Пятница": 05,
-    "Суббота": 06
+	"Понедельник": 01,
+	"Вторник": 02,
+	"Среда": 03,
+	"Четверг": 04,
+	"Пятница": 05,
+	"Суббота": 06
 };
 
 const WEEKODDEVEN = {
@@ -25,14 +26,14 @@ const WEEKODDEVEN = {
 	"ЧЕТНАЯ НЕДЕЛЯ": 02
 }
 
-function parseEvent(inputArray) {
+function parseEvents(inputArray) {
 	const arr = inputArray.replace(/\s+$/gm, '')
-	.replace(/\u0020+/gm, ' ')
-	.replace(/\t+/gm, ";\t")
-	.replace(/^(.*?\;){3}\t\w$/gm, "")
-	.split('\n')
-	.filter(word => word.length > 0)
-	.map(word => word.split(";\t"));
+		.replace(/\u0020+/gm, ' ')
+		.replace(/\t+/gm, ";\t")
+		.replace(/^(.*?\;){3}\t\w$/gm, "")
+		.split('\n')
+		.filter(word => word.length > 0)
+		.map(word => word.split(";\t"));
 
 	for (const key in arr) {
 		if (Object.hasOwnProperty.call(arr, key)) {
@@ -62,7 +63,7 @@ function parseEvent(inputArray) {
 		const date = [
 			STARTDATE[0],
 			STARTDATE[1],
-			STARTDATE[2] + el[1] + (el[0] - 1)*7,
+			STARTDATE[2] + el[1] + (el[0] - 1) * 7,
 		].map(el => el > 9 ? "" + el : "0" + el)
 
 		el[2] = [
@@ -70,23 +71,82 @@ function parseEvent(inputArray) {
 			new Date(`${date.join('-')}T${SCHEDULE[el[2]][0]}${TIMEZONE}`),
 			new Date(`${date.join('-')}T${SCHEDULE[el[2] + dur][1]}${TIMEZONE}`),
 		]
-		el.splice(0,2);
-		el.splice(1,1);
+		el.splice(0, 2);
+		el.splice(1, 1);
 	})
 
 	return arr
 };
 
+function timeStamp(str) {
+	return [
+		str.getYear() + 1900,
+		str.getMonth()+1,
+		str.getDate(),
+		"T",
+		str.getHours(),
+		str.getMinutes(),
+		str.getSeconds(),
+		"Z"
+	].map(el => el > 9 || typeof(el) != 'number' ? "" + el : "0" + el)
+	.join("")
+}
+
+function iCalParser(arrayElement) {
+	const day = {
+		0: "SU",
+		1: "MO",
+		2: "TU",
+		3: "WE",
+		4: "TH",
+		5: "FR",
+		6: "SA"
+	}
+	const string = arrayElement;
+	const DTSTART = timeStamp(string[0][1]);
+	const DTEND = timeStamp(string[0][2]);
+	const UNTIL = ENDPERIOD;
+	const BYDAY = day[string[0][1].getDay()];
+	const DESCRIPTION = [
+		string[1][1],
+		string[1][2],
+		string[1][3]
+	].join('\\n');
+	const LOCATION = string[1][3];
+	const SUMMARY = string[1][0];
+
+	return (
+`BEGIN:VEVENT
+	DTSTART;TZID=Europe/Samara:${DTSTART}
+	DTEND;TZID=Europe/Samara:${DTEND}
+	RRULE:FREQ=WEEKLY;UNTIL=${UNTIL};INTERVAL=2;BYDAY=${BYDAY}
+	DTSTAMP:20210210T202950Z
+	UID:1vkos60b34dt243i4q3clnk86i@google.com
+	CREATED:20180128T102236Z
+	DESCRIPTION:${DESCRIPTION}
+	LAST-MODIFIED:20180128T102236Z
+	LOCATION:${LOCATION}
+	SEQUENCE:0
+	STATUS:CONFIRMED
+	SUMMARY:${SUMMARY}
+	TRANSP:OPAQUE
+END:VEVENT
+`)
+}
+
 async function readFile(filePath) {
-    try {
-        const data = await fs.readFile(filePath);
-        let output = parseEvent(data.toString());
+	try {
+		const data = await fs.readFile(filePath);
+		let output = parseEvents(data.toString());
+		output.map(el => {
+			console.log(iCalParser(el))
+		})
 
-        console.log(output);
+		// console.log(output);
 
-    } catch (error) {
-        console.error(`Got an error trying to read the file: ${error.message}`);
-    }
+	} catch (error) {
+		console.error(`Got an error trying to read the file: ${error.message}`);
+	}
 };
 
 readFile('demofile.txt')
